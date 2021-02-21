920840611e8114b7b2196cf77d8da3f691718a0b
YYI4403 <YYI4403@192.168.1.1>
2021-02-21 01:58:38 +0100

    rrr

--- src/main/java/com/domain/users/UsersApplication.java
+++ src/main/java/com/domain/users/UsersApplication.java
@@ -1,7 +1,10 @@
 package com.domain.users;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,8 +13,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableScheduling
 @EnableWebMvc
 @EnableJms
-public class UsersApplication {
+public class UsersApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UsersApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
